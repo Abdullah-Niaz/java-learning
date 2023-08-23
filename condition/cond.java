468fff483d59abd50c7f01af25d725e229404809
Abdullah Niaz <abdullahniaz423@gmail.com>
2023-08-23 15:52:45 +0500

    conditions if & else if, else

---
+++ condition/cond.java
@@ -0,0 +1,20 @@
+package condition;
+import java.util.*;
+public class cond{
+    public static void main(String[] arr) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        if(a == 1 ){
+            System.out.print("AB1");
+        }
+        else if(a == 2 ){
+            System.out.print("AB2");
+        }
+        else if(a == 3 ){
+            System.out.print("AB3");
+        }
+        else{
+            System.out.print("Invalid Button");
+        }
+    }   
+}
