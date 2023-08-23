08d3e2da7a25bdf1db1daf8a6c38af892477c275
Abdullah Niaz <abdullahniaz423@gmail.com>
2023-08-23 16:09:35 +0500

    swithc statements

---
+++ condition/sw.java
@@ -0,0 +1,25 @@
+package condition;
+import java.util.*;
+
+public class sw {    
+    public static void main(String[] arr) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        switch (a) {
+            case 1:
+                System.out.print("AB1");
+                break;
+        
+            case 2:
+                System.out.print("AB2");
+                break;
+        
+            case 3:
+                System.out.print("AB3");
+                break;
+                
+            default:
+                System.out.print("Invalid input");
+                break;
+        }
+}}
