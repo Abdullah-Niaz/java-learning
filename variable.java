01433800a475717a59104e069205b83e7c91848a
Abdullah Niaz <abdullahniaz423@gmail.com>
2023-08-23 12:31:37 +0500

    variables in java

--- variable.java
+++ variable.java
@@ -1,4 +1,10 @@
 public class variable {
-    public static void main(String[] arr){}
-    
+    public static void main(String[] arr) {
+        int a = 10;
+        int b = 20;
+        int cal = (a + b) / (a - b);
+        System.out.print("Value of Cal is: ");
+        System.out.print(cal);
+    }
+
 }
