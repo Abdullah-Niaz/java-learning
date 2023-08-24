790ba15d300d5c97b77a84666b8940b7de5f138c
Abdullah Niaz <abdullahniaz423@gmail.com>
2023-08-24 23:56:10 +0500

    updated

--- oop/rectangle.java
+++ oop/rectangle.java
@@ -1,10 +1,24 @@
 package oop;
-
 public class rectangle {
     int w,h;
-
-    void perimeter(){
+    int area(){
+        return w * h;
+    }
+    int perimeter(){
         return 2 + ( h + w);
     }
     
+};
+public class MainClass {
+    public static void main(String[] args) {
+        rectangle rect = new rectangle();
+        rect.w = 5;
+        rect.h = 3;
+
+        int area = rect.area();
+        int perimeter = rect.perimeter();
+
+        System.out.println("Rectangle Area: " + area);
+        System.out.println("Rectangle Perimeter: " + perimeter);
+    }
 }
