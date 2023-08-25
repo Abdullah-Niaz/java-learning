1e3dc1b2b476ce43fcfcf0e17ff65184ba8cf008
Abdullah Niaz <abdullahniaz423@gmail.com>
2023-08-25 10:34:34 +0500

    inverted right half pyramid

--- patterns/inverted_right_half.java
+++ patterns/inverted_right_half.java
@@ -2,14 +2,14 @@ package patterns;
 
 import java.util.Scanner;
 
-public class half_pyramid {
+public class inverted_right_half {
         public static void main(String[] arr){
         Scanner s = new Scanner(System.in);
         System.out.print("Enter the Numbers: ");
         int a  = s.nextInt();
-        for(int i = a ; i >= 1 ; i--){
-            for(int j = 1 ;j <= i ; j++){
-                System.out.print(" * ");
+        for(int i = 1 ; i<=a ; i++){
+            for(int j = 1 ;j <= a - i +1 ; j++){
+                System.out.print(j+" ");
             }
             System.out.println();
         }
