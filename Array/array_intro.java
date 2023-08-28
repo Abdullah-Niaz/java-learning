d88449edb6eadcda81b4e45855ab91535d22c760
Abdullah Niaz <abdullahniaz423@gmail.com>
2023-08-28 23:08:10 +0500

    adding array elements

--- Array/array_intro.java
+++ Array/array_intro.java
@@ -3,7 +3,13 @@ package Array;
 public class array_intro {
     public static void main(String[] arr){
         int ar[] = new int[5];
+        ar[0] = 20;
         ar[1] = 20;
+        ar[2] = 20;
+        ar[3] = 20;
+        ar[4] = 20;
+
+        System.out.print(ar[0]);
     }
     
 }
