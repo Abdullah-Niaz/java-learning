5abe22e89f90f8b70bb5e18f7e0e84987dcfbdb3
Abdullah Niaz <abdullahniaz423@gmail.com>
2023-08-28 23:12:01 +0500

    looping through the array

--- Array/array_intro.java
+++ Array/array_intro.java
@@ -4,12 +4,13 @@ public class array_intro {
     public static void main(String[] arr){
         int ar[] = new int[5];
         ar[0] = 20;
-        ar[1] = 20;
-        ar[2] = 20;
-        ar[3] = 20;
-        ar[4] = 20;
-
-        System.out.print(ar[0]);
+        ar[1] = 30;
+        ar[2] = 40;
+        ar[3] = 50;
+        ar[4] = 60;
+        for(int i = 0; i < 5; i++){
+            System.out.println(ar[i]);
+        }
     }
     
 }
