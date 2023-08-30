5710202899413c226c5a6c3da0de86351150e469
Abdullah Niaz <abdullahniaz423@gmail.com>
2023-08-30 19:26:21 +0500

    substing of strings

---
+++ Strings/sbustr.java
@@ -0,0 +1,11 @@
+package Strings;
+
+public class sbustr {
+    public static void main(String[] args){
+        String a = "Hello";
+        String n = a.substring(0,3);
+        System.out.print(n);
+
+        
+    }
+}
