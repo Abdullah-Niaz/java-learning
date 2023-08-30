5de297f12defecba11ed019cc6a16d0a40f28005
Abdullah Niaz <abdullahniaz423@gmail.com>
2023-08-30 19:18:49 +0500

    string comparison using the + operators

--- Strings/intro.java
+++ Strings/intro.java
@@ -2,16 +2,23 @@ package Strings;
 
 public class intro {
     public static void main(String[] args){
-        String a = "Hello World";
-        String b = "hello world";
+        String a = "Hello";
+        String b = "Hello";
 
-        if (a.compareTo(b) == 0){
-            System.out.println("Strings are Equal");
-        }
-        else{
-            System.out.println("Strings are not Equal");
-
-        }
+        // if (a.compareTo(b) == 0){
+        //     System.out.println("Strings are Equal");
+        // }
+        // else{
+        //     System.out.println("Strings are not Equal");
+        
+        // }
+        
+        if (a == b ){
+                System.out.println("Strings are Equal");
+            }
+            else{
+                System.out.println("Strings are not Equal");
+            }
     }
     
 }
