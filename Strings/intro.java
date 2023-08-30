7cb7aac0ca66d8a3caeaa89c6d4020b9cb13777e
Abdullah Niaz <abdullahniaz423@gmail.com>
2023-08-30 19:17:12 +0500

    comparing of string using builtin  method of stings

---
+++ Strings/intro.java
@@ -0,0 +1,17 @@
+package Strings;
+
+public class intro {
+    public static void main(String[] args){
+        String a = "Hello World";
+        String b = "hello world";
+
+        if (a.compareTo(b) == 0){
+            System.out.println("Strings are Equal");
+        }
+        else{
+            System.out.println("Strings are not Equal");
+
+        }
+    }
+    
+}
