dc9b73ee1e28e484525a497dc0f72afa7bc6b19a
Abdullah Niaz <abdullahniaz423@gmail.com>
2023-09-02 07:00:14 +0500

    Ternary Operators

---
+++ Basic/TernaryOperator.java
@@ -0,0 +1,9 @@
+package Basic;
+
+public class TernaryOperator {
+    public static void main(String [] args){
+        int n = 10;
+        int result = n % 2 == 0 ? 10 : 30;
+        System.out.println(result);
+    }    
+}
---
+++ Basic/Type_Conversion.java
@@ -0,0 +1,14 @@
+package Basic;
+
+public class Type_Conversion {
+    public static void main(String[] args){
+        byte a = 20;
+        int b = (int)a;
+        System.out.print(b);
+        char c = 'c';
+        c++;
+        System.out.print(c);
+        
+
+    }
+}
