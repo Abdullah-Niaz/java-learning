4b3d387f08175a32d154c2d3a8c182c808a2ca98
Abdullah Niaz <abdullahniaz423@gmail.com>
2023-09-02 07:01:16 +0500

    Ternary Operators

--- Basic/TernaryOperator.java
+++ Basic/TernaryOperator.java
@@ -3,6 +3,9 @@ package Basic;
 public class TernaryOperator {
     public static void main(String [] args){
         int n = 10;
+        // TernaryOperator:
+            // ? Before, if the condtion is true.
+            // : After, if the condtion is false.
         int result = n % 2 == 0 ? 10 : 30;
         System.out.println(result);
     }    
