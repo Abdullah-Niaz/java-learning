848d2033b1093261bbf81b5f0dd6718dcbc671d2
Abdullah Niaz <abdullahniaz423@gmail.com>
2023-09-04 13:14:03 +0500

    Array objects

---
+++ Array/Array_Objects.java
@@ -0,0 +1,14 @@
+package Array;
+
+import java.util.*;
+
+public class Array_Objects {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        Student st = new Sudent();
+        st.roll = 20;
+
+        Student Students = new Students[4];
+    }
+}
