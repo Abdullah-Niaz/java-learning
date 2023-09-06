12e1192a48275c9f1d8a701e8db2ace46dd0e90f
Abdullah Niaz <abdullahniaz423@gmail.com>
2023-09-07 02:15:24 +0500

    This keyword

--- oop/this_Keyword.java
+++ oop/this_Keyword.java
@@ -1,5 +1,23 @@
 package oop;
+class main{
+    private int n;
+
+    public void setN(int n){
+        // instance variable = local variable 
+        // in order to differentiate we have to use the this keyword
+        //  otherwise compiler will get confused what to do.
+        this.n = n;
+    }
+    public int getN(){
+        return n;
+    }
+
+}
 
 public class this_Keyword {
-    
+public static void main(String[] args){
+    main m = new main();
+    m.setN(20);
+    System.out.println(m.getN());
+}   
 }
