d57cb550e03a2b287344312041e23cb4b99aa308
Abdullah Niaz <abdullahniaz423@gmail.com>
2023-09-06 09:57:24 +0500

    Static Methods

--- Basic/static_Method.java
+++ Basic/static_Method.java
@@ -1,8 +1,28 @@
 package Basic;
+class main{
+    int n;
+    static int m;
+    public void show(){
+        System.out.println("Value of n: "+n);
+    }
+    public static void show1(){
+        m += 1;
+        System.out.println("Value of n: "+m);
+    }
+}
 
 public class static_Method {
     public static void main(String[] args){
-        
+        main obj1 = new main();
+        main obj2 = new main();
+        obj1.n = 20;
+        main.m = 30;
+        obj1.show(); 
+        main.show1(); // 31
+        main.show1(); // 32
+        main.show1(); // 33
+        main.show1(); // 34
+        main.show1(); // 35
     }
     
 }
