f039029b33bf54779db54affd4031379797fd770
Abdullah Niaz <abdullahniaz423@gmail.com>
2023-09-06 09:56:37 +0500

    Static Variable

--- Basic/Static_Variable.java
+++ Basic/Static_Variable.java
@@ -2,6 +2,7 @@ package Basic;
 
 class MyClass {
     public static int count; // Static variable
+    public static String name;
     
     public MyClass() {
         count++; // Increment count whenever an instance is created
@@ -12,7 +13,10 @@ public class Static_Variable {
     public static void  main(String [] args){
         MyClass obj1 = new MyClass();
         MyClass obj2 = new MyClass();
-        
+        MyClass.name="abdullah";
+        System.out.println(MyClass.name);
+        MyClass.name="Omer";
+        System.out.println(MyClass.name);
         System.out.println("Number of instances created: " + MyClass.count); // Accessing the static variable
 
     }
