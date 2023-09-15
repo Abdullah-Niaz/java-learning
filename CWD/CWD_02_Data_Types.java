ac3f2000975ac3dbe721969ecae6b0e6f7b8784b
Abdullah Niaz <abdullahniaz423@gmail.com>
2023-09-16 01:05:55 +0500

    Data types

---
+++ CWD/CWD_02_Data_Types.java
@@ -0,0 +1,22 @@
+package CWD;
+
+import java.util.*;
+
+public class CWD_02_Data_Types {
+    public static void main(String[] args) {
+        
+        int a = 20;
+        System.out.println("Value of Respective Data Types: " + a);
+        char b = 'b';
+        System.out.println("Value of Respective Data Types: " + b);
+        float c = 20;
+        System.out.println("Value of Respective Data Types: " + c);
+        byte d = 20;
+        System.out.println("Value of Respective Data Types: " + d);
+        long f = 234342;
+        System.out.println("Value of Respective Data Types: " + f);
+        String g = "Hello World";
+        System.out.println("Value of Respective Data Types: " + g);
+
+    }
+}
