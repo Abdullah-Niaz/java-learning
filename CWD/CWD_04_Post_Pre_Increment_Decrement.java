aa74fbdf5a19dc54257cfd2c5bab0b44f58494b8
Abdullah Niaz <abdullahniaz423@gmail.com>
2023-09-16 01:53:03 +0500

    increment & Decrement Operators

---
+++ CWD/CWD_04_Post_Pre_Increment_Decrement.java
@@ -0,0 +1,10 @@
+package CWD;
+
+public class CWD_04_Post_Pre_Increment_Decrement {
+    public static void main(String[]args){
+        int y = 7;
+        int x = ++y * 8;
+        System.out.println("value of x: "+x);
+    }
+    
+}
