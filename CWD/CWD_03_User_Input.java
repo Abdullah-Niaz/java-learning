d2789eb3198c1da711c7184f678eba66f9a69800
Abdullah Niaz <abdullahniaz423@gmail.com>
2023-09-16 01:07:05 +0500

    User inputs

---
+++ CWD/CWD_03_User_Input.java
@@ -0,0 +1,11 @@
+package CWD;
+
+import java.util.Scanner;
+
+public class CWD_03_User_Input {
+    public static void main(String[]args){
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        System.out.println("Value of a: " + a);
+    }
+}
