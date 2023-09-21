9d06539ecfaae0d6cfaeb1e1756c3976863968d1
Abdullah-Niaz <abdulahniaz423@gmail.com>
2023-09-21 05:10:49 +0500

    inner Class

--- oop/innerClass.java
+++ oop/innerClass.java
@@ -1,23 +1,33 @@
-package OOP;
+package oop;
 
-class A{
+class A_Ounter{
 
-    int age;
+    int age = 20;
     public void show (){
         System.out.println("In show Outer Class");
     }
 
-    class B{
-        public void show(){
+    static class B_Inner{
+        public void Configshow(){
             System.out.println("In show Inner Class");
         }
     }
 
 }
 
-public class innerClass{
-    public static void main(String[]args){
-        A obj = new A();
-        System.out.println(obj.age);
+
+public class innerClass {
+        public static void main(String[]args){
+        A_Ounter obj = new A_Ounter();
         obj.show();
+
+        // Calling the inner Class
+        // A_Ounter.B_Inner obj1 = obj.new B_Inner();
+        // obj1.Configshow();
+
+
+        // if you wanna you the static, do also 
+        A_Ounter.B_Inner obj1 = new A_Ounter.B_Inner();
+        obj1.Configshow();
     }
+}
