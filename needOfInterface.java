439df06d81e2ae69b9c96ae37bfa225fc78d3756
Abdullah-Niaz <abdullahniaz423@gmail.com>
2023-09-22 23:53:18 +0500

    need of interface

--- needOfInterface.java
+++ needOfInterface.java
@@ -1,28 +1,25 @@
-package oop;
-
-
 // Developer have to work, does not matter it's desktop or computer
 class computer{
-    public void code(){
+    public void code_(){
 
     }
 }
 
 class laptop extends computer{
-    public void code(){
+    public void code_(){
         System.out.println("Code, Run, Compile with Normal Speed");
     }
 }
 
 class desktop extends computer{
-    public void code(){
+    public void code_(){
         System.out.println("Code, Run, Compile with High Speed");
     }
 }
 
 class Developer{
     public void devApp(computer com){
-        com.code();
+        com.code_();
     }
 }
 
@@ -31,11 +28,11 @@ public class needOfInterface{
     public static void main(String [] args){
         computer lap = new laptop();
         computer desk = new desktop();
-        
+
         Developer dev = new Developer();
 
 
-        dev.devApp(lap);
+        // dev.devApp(lap);
         dev.devApp(desk);
 
     }
