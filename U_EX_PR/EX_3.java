853fe6b01d6882e3f47ab7ae24d1e69324685921
Abdullah-Niaz <abdullahniaz423@gmail.com>
2023-09-23 13:14:06 +0500

    EX_3

---
+++ U_EX_PR/EX_3.java
@@ -0,0 +1,38 @@
+package U_EX_PR;
+class Pet {
+    public String speak()  { 
+    return "Hi, I'm a happy and contented pet"; 
+  }
+}
+
+class Dog extends Pet {
+  public String speak()  { 
+    return super.speak() + ". Arf, Arf"; 
+  }
+}
+class Cat extends Pet {
+  public String speak()  { 
+    return super.speak() + ". Meow, meow"; 
+  }
+}
+class Flamingo extends Pet {
+  public String speak() { 
+    return "They're holding me captive. You must help!"; 
+  }
+}
+
+public class EX_3 {
+    public static void main(String [] args){
+        Pet p = new Pet();
+        System.out.println(p.speak());
+        
+        Pet p1 = new Cat();
+        System.out.println(p1.speak());
+
+        Pet p2 = new Dog();
+        System.out.println(p2.speak());
+        
+        Pet p3 = new Flamingo();
+        System.out.println(p3.speak());
+    }
+}
