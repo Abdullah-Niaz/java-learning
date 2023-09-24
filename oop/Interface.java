fc9793801e694bc9eb9f50f02681c94fad81631c
Abdullah-Niaz <abdullahniaz423@gmail.com>
2023-09-25 02:10:03 +0500

    variables inside interfaces

--- oop/Interface.java
+++ oop/Interface.java
@@ -3,6 +3,11 @@ package oop;
 // interface
 interface Animal {
 
+    // ! Varible are static and final in interface, we have to initilize at the time of declaration 
+    // Final : It can't be changed once it is declared
+    String name = "Ali";
+    int age = 20;
+
     // Every method in interface is by default public and static
     void animalSound(); // interface method (does not have a body)
 
@@ -28,5 +33,10 @@ public class Interface {
         System.out.println();
         a.run();
 
+        System.out.println();
+
+        //! Variables can't be changed, it can only be accessed
+        System.out.println(a.name);
+        System.out.println(a.age);
     }
 }
