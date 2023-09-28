cbf9b38f5f4b2c8324610bc08c25071eee7bd2dd
Abdullah-Niaz <abdullahniaz423@gmail.com>
2023-09-28 10:19:02 +0500

    Array

---
+++ CWD/CWD_13_Array.java
@@ -0,0 +1,33 @@
+package CWD;
+
+import loops.for_each;
+
+public class CWD_13_Array {
+    public static void main(String[]args){
+        // Declaration & initilizatino
+        int marks[] = new int[5];
+        marks[0] = 100;
+        marks[1] = 110;
+        marks[2] = 120;
+        marks[3] = 130;
+        marks[4] = 140;
+    for(int i = 0; i < marks.length;i++){
+        System.out.println(marks[i]);
+    }
+
+
+    // Declaration
+    int st[];
+    // initilization
+    st = new int[5];
+
+
+
+    // At the same time
+    int stu[] = {10,20,30,40,50};
+    for(int i =0; i < st.length; i++){
+        System.out.println(stu[i]);
+    }
+
+}   
+}
