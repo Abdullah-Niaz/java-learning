8c268f305b7a5a6215c35ad81b44ab6fe4039214
Abdullah-Niaz <abdullahniaz423@gmail.com>
2023-10-04 23:05:40 +0500

    Calling Methods

---
+++ U_EX_PR/Mid_Term_Exam_Preparation/Calling_Method.java
@@ -0,0 +1,12 @@
+package U_EX_PR.Mid_Term_Exam_Preparation;
+
+public class Calling_Method {
+    public void call(){
+        System.out.println("Hello World");
+    };
+    public static void main(String[] args){
+        Calling_Method c = new Calling_Method();
+        c.call();
+    }
+    
+}
