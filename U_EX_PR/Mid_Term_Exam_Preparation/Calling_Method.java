bcfe5bd9bf8ab060ab30e9c48780b1d5b6361fce
Abdullah-Niaz <abdullahniaz423@gmail.com>
2023-10-04 23:13:04 +0500

    Method Calling

--- U_EX_PR/Mid_Term_Exam_Preparation/Calling_Method.java
+++ U_EX_PR/Mid_Term_Exam_Preparation/Calling_Method.java
@@ -1,12 +1,21 @@
 package U_EX_PR.Mid_Term_Exam_Preparation;
 
+import java.text.AttributedCharacterIterator.Attribute;
+
 public class Calling_Method {
+    // Attributes / Variables / instance Variable
+    String fname = "John";
+    String lname = "Doe";
+    int age = 24;
+
     public void call(){
         System.out.println("Hello World");
     };
     public static void main(String[] args){
-        Calling_Method c = new Calling_Method();
-        c.call();
+        Calling_Method myObj = new Calling_Method();
+        System.out.println("Name: " + myObj.fname + " " + myObj.lname);
+        System.out.println("Age: " + myObj.age);
+        myObj.call();
     }
     
 }
