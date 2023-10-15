6c649519a3283056b45c5eddefc103a705b92d3c
Abdullah-Niaz <abdullahniaz423@gmail.com>
2023-10-15 11:02:37 +0500

    test file

---
+++ Abdul_Bari_Java/Object_Oriented_Programming_11/Constructors/test.java
@@ -0,0 +1,14 @@
+package Abdul_Bari_Java.Object_Oriented_Programming_11.Constructors;
+
+class tests{
+    tests(){
+        System.out.println("tests constucutor 1");
+    }
+    
+}
+
+public class test {
+    public static void main(String[] args){
+
+    }
+}
