0c9504b4d4f12accf13037eac8a0ef121fb15a5b
Abdullah-Niaz <abdullahniaz423@gmail.com>
2023-10-17 22:14:38 +0500

    Constructors default

---
+++ Abdul_Bari_Java/Object_Oriented_Programming_11/Constructors/construtor.java
@@ -0,0 +1,40 @@
+// package Abdul_Bari_Java.Object_Oriented_Programming_11.Constructors;
+
+// class Parent{
+//     public Parent(){
+//         System.out.println("Parent Constructor");
+//     }
+// }
+// class Child extends Parent{
+//     public Child(){
+//         System.out.println("Child Constructor");
+//     }
+// }
+
+
+// public class construtor {
+//     public static void main(String[]args){
+//         Child c = new Child();
+//     }
+    
+// }
+// package Abdul_Bari_Java.Object_Oriented_Programming_11.Constructors;
+
+// class Parent{
+//     public Parent(){
+//         System.out.println("Parent Constructor");
+//     }
+// }
+// class Child extends Parent{
+//     public Child(){
+//         System.out.println("Child Constructor");
+//     }
+// }
+
+
+// public class construtor {
+//     public static void main(String[]args){
+//         Child c = new Child();
+//     }
+    
+// }
